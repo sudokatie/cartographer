1636f1e9c9ca488a589a455e15ddd49a8f10872c
Katie <everthejetsets@gmail.com>
2026-02-09 14:05:05 -0500

    add go parser support

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,11 +1,13 @@
 // Parser module for extracting AST from source files
 
 pub mod ast;
+mod go;
 mod javascript;
 mod python;
 mod rust;
 
 pub use ast::*;
+pub use go::GoParser;
 pub use javascript::{JavaScriptParser, JsVariant};
 pub use python::PythonParser;
 pub use rust::RustParser;
